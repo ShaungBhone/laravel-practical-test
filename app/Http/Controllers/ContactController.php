3dbec5d751511b1f104b261a4733e5c9cdd9a7e9
ShaungBhone <phonelaayy@gmail.com>
2023-04-30 15:30:02 +0630

    feature - customer contact form

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'nullable|string',
+            'dob' => 'nullable|date',
+            'phone' => 'nullable|numeric|regex:/^([0-9\s\-\+\(\)]*)$/|min:5',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors(),
+                ],
+                400,
+            );
+        }
+
+        $contact = new Contact();
+        $contact->name = $request->input('name');
+        $contact->dob = $request->input('dob');
+        $contact->phone = $request->input('phone');
+        $contact->save();
+
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $contact,
+            ],
+            201,
+        );
+    }
+}
